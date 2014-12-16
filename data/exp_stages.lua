9bdf2c32b6ab0235511087386ea8d5f5b68e98dd
Tatuy <tatuy_@msn.com>
2014-12-16 01:37:58 -0700

    Archivo precompilado, config base, stages y mapa
    Se agregó el mapa original de guia3d 822.
    Se agregó el archivo precompilado debian 7 (x64).
    Se modificó el archivo exp_stages.lua agregando al average 7x
    (considerar el 3x de guia3d).
    Se modificó config.lua agregando la configuración inicial base.

--- data/exp_stages.lua
+++ data/exp_stages.lua
@@ -1,8 +1,21 @@
 --if you want to enable exp_stages, make sure that you have experience_stages = true at your config.lua
-
+-- average 7x (3x g3dmonsters)
 stages = {
-	{minLevel = 1, maxLevel = 40, multiplier = 60},
-	{minLevel = 41, maxLevel = 60, multiplier = 50},
-	{minLevel = 61, maxLevel = 80, multiplier = 40},
-	{minLevel = 81, maxLevel = 100, multiplier = 30}
+	{minLevel = 1, maxLevel = 7, multiplier = 20},
+	{minLevel = 8, maxLevel = 24, multiplier = 17.5},
+	{minLevel = 25, maxLevel = 49, multiplier = 15},
+	{minLevel = 50, maxLevel = 59, multiplier = 12.5},
+	{minLevel = 60, maxLevel = 69, multiplier = 10},
+	{minLevel = 70, maxLevel = 79, multiplier = 9},
+	{minLevel = 80, maxLevel = 89, multiplier = 8},
+	{minLevel = 90, maxLevel = 99, multiplier = 7},
+	{minLevel = 100, maxLevel = 109, multiplier = 6},
+	{minLevel = 110, maxLevel = 119, multiplier = 4.5},
+	{minLevel = 130, maxLevel = 139, multiplier = 4},
+	{minLevel = 140, maxLevel = 149, multiplier = 3.5},
+	{minLevel = 150, maxLevel = 159, multiplier = 3},
+	{minLevel = 160, maxLevel = 169, multiplier = 2.5},
+	{minLevel = 170, maxLevel = 179, multiplier = 2},
+	{minLevel = 180, maxLevel = 199, multiplier = 1.5},
+	{minLevel = 200, maxLevel = 999, multiplier = 1}
 }
