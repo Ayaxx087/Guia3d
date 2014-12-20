17eab664eb830c8a104d28bcc9c1c85525063223
Tatuy <tatuy_@msn.com>
2014-12-20 16:35:42 -0700

    Free premium for level 60+

--- data/creaturescripts/scripts/login.lua
+++ data/creaturescripts/scripts/login.lua
@@ -28,15 +28,15 @@ function onLogin(cid)
 		--doPlayerSetLossPercent(cid, PLAYERLOSS_ITEMS, 10)
 		--doPlayerSetLossPercent(cid, PLAYERLOSS_CONTAINERS, 100)
 		
-		--Free premium para todos
-		--[[
-		if (isPremium(cid) == false) then
+		
+		--[[ Free premium para todos los nivel 60+ ]]
+		if (isPremium(cid) == false and getPlayerLevel(cid) > 59) then
 				local name = getCreatureName(cid)
 				local player = getPlayerByName(name)
 				doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You have received 30 days of premium account. Please login again.")
 				doPlayerAddPremiumDays(cid, player, 30)
 		end
-		]]
+		
 		
 		--Remover privilegio de premia city
 		--if (isPremium(cid) == false and getPlayerTown(cid) == 21 or getPlayerTown(cid) == 22) then
