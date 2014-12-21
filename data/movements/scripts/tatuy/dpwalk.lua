2aee2e94850291db6c0fc2b1af4b1dd00a383031
Tatuy <tatuy_@msn.com>
2014-12-20 19:31:40 -0700

    Fixed dpwalk.lua
    - Retransformar tile del depot.

--- data/movements/scripts/tatuy/dpwalk.lua
+++ data/movements/scripts/tatuy/dpwalk.lua
@@ -5,8 +5,73 @@ function onStepIn(cid, item, pos, frompos)
 				frompos = getPlayerMasterPos(cid)
 			end
 
+			doTransformSwitchTile(item) --Revertir el aplastamiento del depot tile
 			doTeleportThing(cid, frompos)
 		--end
 	end
 	return true
+end
+
+function doTransformSwitchTile(item)
+	if isInArray(SWITCH_TILE_ON, item.itemid) == TRUE then
+		if item.itemid == 425 then
+			doTransformItem(item.uid, item.itemid + 1)
+		else
+			doTransformItem(item.uid, item.itemid - 1)
+		end
+	else
+		if item.itemid == 426 then
+			doTransformItem(item.uid, item.itemid - 1)
+		else
+			doTransformItem(item.uid, item.itemid + 1)
+		end
+	end
+end
+
+function doTransformSwitchTile(item)
+	if isInArray(SWITCH_TILE_ON, item.itemid) == TRUE then
+		if item.itemid == 416 then
+			doTransformItem(item.uid, item.itemid + 1)
+		else
+			doTransformItem(item.uid, item.itemid - 1)
+		end
+	else
+		if item.itemid == 417 then
+			doTransformItem(item.uid, item.itemid - 1)
+		else
+			doTransformItem(item.uid, item.itemid + 1)
+		end
+	end
+end
+
+function doTransformSwitchTile(item)
+	if isInArray(SWITCH_TILE_ON, item.itemid) == TRUE then
+		if item.itemid == 446 then
+			doTransformItem(item.uid, item.itemid + 1)
+		else
+			doTransformItem(item.uid, item.itemid - 1)
+		end
+	else
+		if item.itemid == 447 then
+			doTransformItem(item.uid, item.itemid - 1)
+		else
+			doTransformItem(item.uid, item.itemid + 1)
+		end
+	end
+end
+
+function doTransformSwitchTile(item)
+	if isInArray(SWITCH_TILE_ON, item.itemid) == TRUE then
+		if item.itemid == 3216 then
+			doTransformItem(item.uid, item.itemid + 1)
+		else
+			doTransformItem(item.uid, item.itemid - 1)
+		end
+	else
+		if item.itemid == 3217 then
+			doTransformItem(item.uid, item.itemid - 1)
+		else
+			doTransformItem(item.uid, item.itemid + 1)
+		end
+	end
 end
