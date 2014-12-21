f6c09a8d58716d45076c557ac70e4db619cf3211
Tatuy <tatuy_@msn.com>
2014-12-20 18:49:40 -0700

    Revert "Updated switch_tile.lua"
    This reverts commit 97e5eb807b6777db14c729f4289782d6f0df3cea.

--- data/movements/scripts/switch_tile.lua
+++ data/movements/scripts/switch_tile.lua
@@ -1,58 +1,94 @@
-local SWITCHES = { {416, 417}, {426, 425}, {446, 447}, {3216, 3217}, {11062, 11063} }
-
-local function transformItemKeepingAid(item, newid)
-	local ret = doTransformItem(item.uid, newid)
-	if ret and item.actionid ~= 0 then
-		doSetItemActionId(item.uid, item.actionid)
-	end
-	return ret
-end
-
-local function doTransformTile(item)
-	local ret
-	for i, v in pairs(SWITCHES) do
-		if(item.itemid == v[1]) then
-			return transformItemKeepingAid(item, v[2])
-		elseif(item.itemid == v[2]) then
-			return transformItemKeepingAid(item, v[1])
-		end
-	end
-end
-
 function onStepIn(cid, item, pos)
-	if(item.actionid > 0) then
+	if isPlayer(cid) ~= TRUE then
 		return true
 	end
-
-	doTransformTile(item)
-	
-	if(isPlayer(cid) ) then
-		local depot = {}
-		for x = -1, 1 do
-			for y = -1, 1 do
-				pos.x = pos.x + x
-				pos.y = pos.y + y
-				depot = getTileItemByType(pos, ITEM_TYPE_DEPOT)
-				if(depot.uid > 0) then
-					local depotItems = getPlayerDepotItems(cid, getDepotId(depot.uid))
-					local depotStr = "Your depot contains " .. depotItems .. " items."
-					if(depotItems == 1) then
-						depotStr = "Your depot contains 1 item."
-					end
-					doPlayerSendTextMessage(cid, MESSAGE_EVENT_DEFAULT, depotStr)
-					return true
-				end
-				-- The pos has changed, change it back
-				pos.x = pos.x - x
-				pos.y = pos.y - y
-			end
+	if item.actionid == 100 then
+		doTransformSwitchTile(item)
+	elseif item.actionid > 100 then
+		if getPlayerDepotItems(cid, item.actionid - 100) > 1 then
+			doPlayerSendTextMessage(cid, 23, "Your depot contains " .. getPlayerDepotItems(cid, item.actionid - 100) .. " items.")
+		else
+			doPlayerSendTextMessage(cid, 23, "Your depot contains 1 item.")
 		end
+		doTransformSwitchTile(item)
 	end
-	
 	return true
 end
 
 function onStepOut(cid, item, pos)
-	doTransformTile(item)
+	doTransformSwitchTile(item)
 	return true
+end
+
+function doTransformSwitchTile(item)
+	if isInArray(SWITCH_TILE_ON, item.itemid) == TRUE then
+		if item.itemid == 425 then
+			doTransformItem(item.uid, item.itemid + 1)
+		else
+			doTransformItem(item.uid, item.itemid - 1)
+		end
+	else
+		if item.itemid == 426 then
+			doTransformItem(item.uid, item.itemid - 1)
+		else
+			doTransformItem(item.uid, item.itemid + 1)
+		end
+	end
+end
+function StepIn(cid, item, pos)
+	doTransformSwitchTile(item)
+	return true
+end
+function doTransformSwitchTile(item)
+	if isInArray(SWITCH_TILE_ON, item.itemid) == TRUE then
+		if item.itemid == 416 then
+			doTransformItem(item.uid, item.itemid + 1)
+		else
+			doTransformItem(item.uid, item.itemid - 1)
+		end
+	else
+		if item.itemid == 417 then
+			doTransformItem(item.uid, item.itemid - 1)
+		else
+			doTransformItem(item.uid, item.itemid + 1)
+		end
+	end
+end
+function StepIn(cid, item, pos)
+	doTransformSwitchTile(item)
+	return true
+end
+function doTransformSwitchTile(item)
+	if isInArray(SWITCH_TILE_ON, item.itemid) == TRUE then
+		if item.itemid == 446 then
+			doTransformItem(item.uid, item.itemid + 1)
+		else
+			doTransformItem(item.uid, item.itemid - 1)
+		end
+	else
+		if item.itemid == 447 then
+			doTransformItem(item.uid, item.itemid - 1)
+		else
+			doTransformItem(item.uid, item.itemid + 1)
+		end
+	end
+end
+function StepIn(cid, item, pos)
+	doTransformSwitchTile(item)
+	return true
+end
+function doTransformSwitchTile(item)
+	if isInArray(SWITCH_TILE_ON, item.itemid) == TRUE then
+		if item.itemid == 3216 then
+			doTransformItem(item.uid, item.itemid + 1)
+		else
+			doTransformItem(item.uid, item.itemid - 1)
+		end
+	else
+		if item.itemid == 3217 then
+			doTransformItem(item.uid, item.itemid - 1)
+		else
+			doTransformItem(item.uid, item.itemid + 1)
+		end
+	end
 end
