d04c64209cb9b882a3fb517152a847ad9ff15b97
Tatuy <tatuy_@msn.com>
2014-12-23 02:06:04 -0700

    Fixed manarune.lua
    Se ha cambiado el return 0 por un return false, y el return 1 por un
    return true en los archivos de spells.

--- data/spells/scripts mm/81/test.lua
+++ data/spells/scripts mm/81/test.lua
@@ -1,5 +1,5 @@
 function onUse(cid, item, frompos, item2, topos)
 
 doCreateTeleport(5024, {Lugar al ke te lleva x=42, y=455, z=7}, {Donde se crea x=46, y=455, z=7})
-     return 1
+     return true
 end
--- data/spells/scripts/manarune.lua
+++ data/spells/scripts/manarune.lua
@@ -4,6 +4,6 @@ function onCastSpell(cid, var)
             doPlayerAddMana(cid, math.random(70, 110))
             doSendAnimatedText(getPlayerPosition(cid), "Aaaah...", 198)
             doSendMagicEffect(getPlayerPosition(cid), 12)
-return 0
+return false
 	
 end
