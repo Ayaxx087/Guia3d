1dd471b1546a093d04a27e14a52915273ea7b3be
Tatuy <tatuy_@msn.com>
2014-12-28 22:06:33 -0700

    Updated weapons
    Carpeta original de otserv.
    - Se removió "level" de todas las armas (menos rods y wands).
    - Se removió "vocation" de todas las armas (menos rods y wands).
    - Se agregó merlin staff, jester staff y el script de explosive_arrow.
    - Se redujo el daño de explosive arrow en un 30%.

--- data/weapons/scripts/sprite wand.lua
+++
@@ -1,8 +0,0 @@
-local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 0, -2000, 0, -3500)
-
-function onUseWeapon(cid, var)
-	return doCombat(cid, combat, var)
-end
