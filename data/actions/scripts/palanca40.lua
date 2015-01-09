d3433c4a4f4493f053ad0ede7015cd51aafcc52f
Tatuy <tatuy_@msn.com>
2015-01-08 20:34:55 -0700

    Se arregló palancas orc room

--- data/actions/scripts/palanca40.lua
+++ data/actions/scripts/palanca40.lua
@@ -1,24 +1,19 @@
-function onUse(cid, item, frompos, item2, topos)
+local doorPos = {x=403, y=411, z=10}
 
-if item.itemid == 1945 then
-
-	doTransformItem(item.uid,1946)
-	doTransformItem(1203,1214)
- else
- 	doTransformItem(item.uid,1945)
-	estatuapos = {x=403, y=411, z=10, stackpos=253}
-	creature = getThingfromPos(estatuapos)
-	if creature.itemid > 0 then
-		estatuapos.y = estatuapos.y + 1
-		doTeleportThing(creature.uid,estatuapos)
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	if item.itemid == 1945 then
+		local v = getTileItemById(doorPos, 1214)
+		if v.itemid == 1214 then
+			doRelocate(doorPos, {x=doorPos.x, y=doorPos.y+1, z=doorPos.z})
+			doTransformItem(v.uid, 1212)
+		end
+		else
+			local v = getTileItemByType(doorPos, ITEM_TYPE_DOOR)
+	if v.itemid == 1212 or v.itemid == 5108 then
+		doTransformItem(v.uid, 1214)
 	end
-
- 	doTransformItem(item.uid,1945)
- 	doTransformItem(1203,1212)
-
- end
-
- return true
+	end
+	return doTransformItem(item.uid, item.itemid == 1945 and 1946 or 1945)
 end
 
 --<action uniqueid="4003" script="palanca40.lua" />---
