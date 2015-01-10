f289f30b643e9274ff2fa9be4c7e6871772eb7a4
Tatuy <tatuy_@msn.com>
2015-01-10 02:12:36 -0700

    Stamina verde rate x6

--- config.lua
+++ config.lua
@@ -204,7 +204,7 @@ height_minimum_for_idle = 3
 rate_stamina_gain = 1002
 -- Slow gain stamina rate. This is used if you are gaining stamina that gives extra experience
 -- 1 second offline = X milliseconds of stamina
-slow_rate_stamina_gain = 300
+slow_rate_stamina_gain = 600
 -- Loss stamina rate - works as other rates
 rate_stamina_loss = 1
 -- For how many milliseconds (from 42 hours) you get extra experience from monsters
