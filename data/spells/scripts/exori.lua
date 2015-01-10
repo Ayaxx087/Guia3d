66495c5eb32c0801f7e03a4afbdbcfd59a49e332
Tatuy <tatuy_@msn.com>
2015-01-10 01:00:35 -0700

    Buffed spell Exori
    High formula damage + 2 + .5.

--- data/spells/scripts/exori.lua
+++ data/spells/scripts/exori.lua
@@ -1,8 +1,7 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_HITAREA)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -3.8, 30, -4.3, 0)
---etCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -2.1, 30, -2.6, 0)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -4.6, 30, -5.1, 0)
 
 local arr = {
 {1, 1, 1},
