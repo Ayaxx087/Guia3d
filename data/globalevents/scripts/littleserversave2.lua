ae0b55f52cdec04193a11a63bc36ee1d6a971a51
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2016-02-16 00:11:09 -0700

    Update serversave system

---
+++ data/globalevents/scripts/littleserversave2.lua
@@ -0,0 +1,4 @@
+function onTime(interval)
+	if doSaveServer(true) then
+	end
+end
---
+++ data/globalevents/scripts/serversave2.lua
@@ -0,0 +1,38 @@
+local shutdownAtServerSave = true
+local cleanMapAtServerSave = false
+
+local function serverSave()
+        if shutdownAtServerSave then
+                doSetGameState(GAME_STATE_SHUTDOWN)
+    else
+        doSetGameState(GAME_STATE_NORMAL)
+        end
+        if cleanMapAtServerSave then
+                cleanMap()
+        end
+        saveServer()
+end
+
+local function secondServerSaveWarning()
+        doTatuyBroadcastMessage("Server is saving game. The server will be open again in a moment.")
+		doSetGameState(GAME_STATE_CLOSED)
+		if doSaveServer(true) then
+			doTatuyBroadcastMessage("SAVED! the server will close in 20 seconds...")
+			saveServer()
+		else
+			doTatuyBroadcastMessage("Server could not be saved. Please report this to an GM.")
+		end
+        addEvent(serverSave, 20000)
+end
+
+local function firstServerSaveWarning()
+        doTatuyBroadcastMessage("Server is saving game in 2 minutes. Please go to a safe place.")
+        addEvent(secondServerSaveWarning, 120000)
+end
+
+function onTime(interval)
+        doTatuyBroadcastMessage("Server is saving game in 4 minutes. Please go to a safe place.")
+        doSetGameState(GAME_STATE_STARTUP)
+        addEvent(firstServerSaveWarning, 120000)
+        return not shutdownAtServerSave
+end
