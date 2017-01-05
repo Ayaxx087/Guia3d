a2724623f8c9fcf873cc4f8ea679fd957fc5f635
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2017-01-05 15:51:47 -0700

    Edit basic configuration
    Edit kills to get red and black skull (x2).

--- config.lua
+++ config.lua
@@ -124,15 +124,15 @@ defensive_pz_lock = true
 
 -- How many players you need to kill to gain a red skull per day/week/month
 -- Set 0 to disable
-kills_per_day_red_skull = 12
-kills_per_week_red_skull = 84
-kills_per_month_red_skull = 360
+kills_per_day_red_skull = 8
+kills_per_week_red_skull = 56
+kills_per_month_red_skull = 240
 
 -- How many players you need to kill to gain a black skull per day/week/month
 -- Set 0 to disable
-kills_per_day_black_skull = 21
-kills_per_week_black_skull = 147
-kills_per_month_black_skull = 630
+kills_per_day_black_skull = 14
+kills_per_week_black_skull = 98
+kills_per_month_black_skull = 420
 
 -- Broadcast violation window banishments
 broadcast_banishments = "no"
