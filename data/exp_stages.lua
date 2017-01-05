9d46ebe535a8806e36de79f8ceb4270f9d256b83
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2017-01-05 07:33:58 -0700

    Change experience stages

--- data/exp_stages.lua
+++ data/exp_stages.lua
@@ -1,12 +1,12 @@
 --if you want to enable exp_stages, make sure that you have experience_stages = true at your config.lua
 stages = {
-	{minLevel = 1, maxLevel = 7, multiplier = 23},
-	{minLevel = 8, maxLevel = 29, multiplier = 20},
-	{minLevel = 30, maxLevel = 49, multiplier = 17},
-	{minLevel = 50, maxLevel = 74, multiplier = 12},
-	{minLevel = 75, maxLevel = 94, multiplier = 7},
-	{minLevel = 95, maxLevel = 119, multiplier = 5},
-	{minLevel = 120, maxLevel = 149, multiplier = 3},
-        {minLevel = 150, maxLevel = 200, multiplier = 2},
-	{minLevel = 201, maxLevel = 1000, multiplier = 1}
+	{minLevel = 1, maxLevel = 20, multiplier = 10},
+	{minLevel = 21, maxLevel = 40, multiplier = 8},
+	{minLevel = 41, maxLevel = 60, multiplier = 7},
+	{minLevel = 61, maxLevel = 80, multiplier = 6},
+	{minLevel = 81, maxLevel = 100, multiplier = 5},
+	{minLevel = 101, maxLevel = 120, multiplier = 4},
+	{minLevel = 121, maxLevel = 140, multiplier = 3},
+	{minLevel = 141, maxLevel = 160, multiplier = 2},
+	{minLevel = 161, maxLevel = 1000, multiplier = 1}
 }
