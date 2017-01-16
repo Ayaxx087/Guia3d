28189249a78f2dbccf994fd0e1d88521e747bce1
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2017-01-15 18:15:43 -0700

    Add POI teleport only for levels 250+

---
+++ data/movements/scripts/tatuy/poi.lua
@@ -0,0 +1,27 @@
+
+function onStepIn(cid, item, pos)
+npos = {x=861, y=825, z=7}
+ if getPlayerLevel(cid) < 250 then
+		doPlayerSendTextMessage(cid,22,"Sorry, you are under lvl 250.")
+                doTeleportThing(cid,npos)
+		return true
+	end
+
+
+        if item.actionid == 55001 then
+		if isPlayer(cid) == TRUE then
+		newpos = {x=1057, y=846, z=10}
+		doPlayerSendTextMessage(cid,22,"Now you are on this journey...")
+		doTeleportThing(cid,newpos)
+		doSendMagicEffect(newpos,12)
+		end
+
+	else
+		return false
+	end
+	return true
+end
+
+function onStepOut(cid, item, pos)
+	return true
+end
