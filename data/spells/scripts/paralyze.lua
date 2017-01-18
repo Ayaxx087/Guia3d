18b8f651f7c60818ede3053fbb19c84d9c669517
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2017-01-18 10:50:43 -0700

    Set Paralyze rune by Guia3d default spell

--- data/spells/scripts/paralyze.lua
+++ data/spells/scripts/paralyze.lua
@@ -1,16 +1,12 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
-setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
 
 local condition = createConditionObject(CONDITION_PARALYZE)
-setConditionParam(condition, CONDITION_PARAM_TICKS, 60000)
+setConditionParam(condition, CONDITION_PARAM_TICKS, 20000)
+--setConditionParam(condition, CONDITION_PARAM_SPEED, -200)
 setConditionFormula(condition, -0.9, 0, -0.9, 0)
 setCombatCondition(combat, condition)
 
 function onCastSpell(cid, var)
-	if doCombat(cid, combat, var) then
-		doSendMagicEffect(getCreaturePosition(cid), CONST_ME_MAGIC_GREEN)
-	end
-
-	return true
+	return doCombat(cid, combat, var)
 end
