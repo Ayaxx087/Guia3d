218c1d2a62da9127c4de390d08b033729837d451
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2017-01-18 09:48:40 -0700

    Move Asgard enter

--- data/movements/scripts/tatuy/asgard.lua
+++ data/movements/scripts/tatuy/asgard.lua
@@ -1,6 +1,6 @@
 
 function onStepIn(cid, item, pos)
-npos = {x=703, y=169, z=7}
+npos = {x=452, y=329, z=7}
  if getPlayerLevel(cid) < 350 then
 		doPlayerSendTextMessage(cid,22,"Sorry, you are under lvl 350.")
                 doTeleportThing(cid,npos)
