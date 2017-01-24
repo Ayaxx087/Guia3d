78bcef68397fe3143d88361d4bf4b44400de109b
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2017-01-23 19:12:37 -0700

    Add Yalahar Teleports

---
+++ data/actions/scripts/tatuy/yalahar/yalahartpnorth2back.lua
@@ -0,0 +1,16 @@
+function onUse(cid, item, frompos, item2, topos)
+
+npos = {x=1809, y=197, z=5}
+		if getPlayerLevel(cid) >= 30 then
+		
+                doTeleportThing(cid,npos)
+                doCreatureSay(cid,"You are joining to the city!",19)
+
+                doSendMagicEffect(npos,12)
+else
+		doPlayerSendCancel(cid, "Sorry you are under lvl 30.")
+end
+
+
+	return true
+end
---
+++ data/actions/scripts/tatuy/yalahar/yalahartpwest1.lua
@@ -0,0 +1,16 @@
+function onUse(cid, item, frompos, item2, topos)
+
+npos = {x=1760, y=252, z=5}
+		if getPlayerLevel(cid) >= 30 then
+		
+                doTeleportThing(cid,npos)
+                doCreatureSay(cid,"You are joining on the dangerous!",19)
+
+                doSendMagicEffect(npos,12)
+else
+		doPlayerSendCancel(cid, "Sorry you are under lvl 30.")
+end
+
+
+	return true
+end
