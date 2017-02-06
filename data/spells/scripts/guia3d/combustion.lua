706fba86db7e49534b4c107023516e73919d7a81
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2017-02-05 20:01:51 -0700

    Update Spells
    *Update formula of guia3d custom spell.

--- data/spells/scripts/guia3d/combustion.lua
+++ data/spells/scripts/guia3d/combustion.lua
@@ -2,7 +2,14 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_FIREAREA)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_FIRE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -2.3, -30, -2.7, 0)
+
+function onGetFormulaValues(cid, level, maglevel)
+	local min = ((level/5)+(maglevel*3))
+	local max = ((level/5)+(maglevel*10))
+	return -min, -max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 local arr = {
 
