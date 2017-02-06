1767b956dd54ea459f828b5b58780c4c48e08689
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2017-02-05 19:28:55 -0700

    Update Spells
    *Changeformula of guia3d spell.

--- data/spells/scripts/guia3d/exina gran mas vis.lua
+++ data/spells/scripts/guia3d/exina gran mas vis.lua
@@ -1,7 +1,14 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_GROUNDSHAKER)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -2.8, 30, -3.5, 0)
+
+function onGetFormulaValues(cid, level, maglevel)
+	local min = ((level/5)+(maglevel*5))
+	local max = ((level/5)+(maglevel*12))
+	return -min, -max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 arr = {
 {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
