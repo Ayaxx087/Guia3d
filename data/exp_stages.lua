228f44fbd8ccd7c3f2bc2296c7a68a2e809c1e00
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2017-02-12 20:00:31 -0700

    Edit rate stages
    From Level To Level Rate
    1 50 150x
    51 70 70x
    71 100 30x
    101 120 10x
    121 140 5x
    141 160 3x
    161 180 2x
    181 - 1x

--- data/exp_stages.lua
+++ data/exp_stages.lua
@@ -1,13 +1,12 @@
 --if you want to enable exp_stages, make sure that you have experience_stages = true at your config.lua
 
 stages = {
-	{minLevel = 1, maxLevel = 20, multiplier = 10},
-	{minLevel = 21, maxLevel = 40, multiplier = 8},
-	{minLevel = 41, maxLevel = 60, multiplier = 7},
-	{minLevel = 61, maxLevel = 80, multiplier = 6},
-	{minLevel = 81, maxLevel = 100, multiplier = 5},
-	{minLevel = 101, maxLevel = 120, multiplier = 4},
-	{minLevel = 121, maxLevel = 140, multiplier = 3},
-	{minLevel = 141, maxLevel = 160, multiplier = 2},
-	{minLevel = 161, maxLevel = 1000, multiplier = 1}
+	{minLevel = 1, maxLevel = 50, multiplier = 150},
+	{minLevel = 51, maxLevel = 70, multiplier = 70},
+	{minLevel = 71, maxLevel = 100, multiplier = 30},
+	{minLevel = 101, maxLevel = 120, multiplier = 10},
+	{minLevel = 121, maxLevel = 140, multiplier = 5},
+	{minLevel = 141, maxLevel = 160, multiplier = 3},
+	{minLevel = 161, maxLevel = 180, multiplier = 2},
+	{minLevel = 181, maxLevel = 1000, multiplier = 1}
 }
