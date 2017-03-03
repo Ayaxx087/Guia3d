6b5c5e719e415f0123f2df0cb6e4feadf1eee03d
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2017-03-02 19:40:23 -0700

    Remove old spells

--- data/_old1_spells/scripts/light_magic_missile.lua
+++
@@ -1,9 +0,0 @@
-local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_EXPLOSIONHIT)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_FIRE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -1.1, 0, -1.2, 0)
-
-function onCastSpell(cid, var)
-	return doCombat(cid, combat, var)
-end
--- data/_old1_spells/scripts/manarune.lua
+++
@@ -1,7 +0,0 @@
-function onCastSpell(cid, var)
-            doPlayerAddMana(cid, math.random(90, 150))
-            doSendAnimatedText(getPlayerPosition(cid), "Aaaah...", 198)
-            doSendMagicEffect(getPlayerPosition(cid), 12)
-return true
-	
-end
