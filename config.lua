3b678eac7e90c6e7e5ee3609b522b7446a40ab96
Jesús Rafael Zúñiga Castro <a211214087@alumnos.uson.mx>
2017-03-20 16:42:27 -0700

    *Evento activado
    ¡loot y exp al doble!

--- config.lua
+++ config.lua
@@ -170,7 +170,7 @@ rate_exp = 1
 rate_exp_pvp = 0
 rate_skill = 3
 rate_mag = 2
-rate_loot = 1
+rate_loot = 2
 rate_spawn = 1
 
 --Displays the skill advanced to when advancing in skills (melee) [default: "no"]
@@ -330,8 +330,8 @@ lua_excepted_type_errors_enabled = false
 guild_wars_end_only_on_startup = true
 
 -- motd (the message box that you sometimes get before you choose characters)
-motd = "Welcome to Ultima Alianza Tibia Guia3d RPG ONLINE: \n \n www.uatibia.com \n\n Follow us in facebook www.facebook.com/uatibia and get the official \n source for news, tips and good assistance for the game.\n Join the community in our forums: http://forums.uatibia.com.\n\n *Have fun! \n\n\n The Administration."
-motdnum = "2"
+motd = "Welcome to Ultima Alianza Tibia Guia3d RPG ONLINE: \n \n www.uatibia.com \n\n Follow us in facebook www.facebook.com/uatibia and get the official \n source for news, tips and good assistance for the game.\n Join the community in our forums: http://forums.uatibia.com.\n\n*Evento activo: ¡loot y exp al doble!\n\n *Have fun! \n\n\n The Administration."
+motdnum = "3"
 
 -- login message
 loginmsg = "Welcome to the UATibia Guia3d RPG online, for get the official source for news and more info, visit: http://www.uatibia.com"
--- data/exp_stages.lua
+++ data/exp_stages.lua
@@ -1,8 +1,8 @@
 --if you want to enable exp_stages, make sure that you have experience_stages = true at your config.lua
 
 stages = {
-	{minLevel = 1, maxLevel = 40, multiplier = 4},
-	{minLevel = 41, maxLevel = 60, multiplier = 3},
-	{minLevel = 61, maxLevel = 80, multiplier = 2},
-	{minLevel = 81, maxLevel = 1000, multiplier = 1}
+	{minLevel = 1, maxLevel = 40, multiplier = 8},
+	{minLevel = 41, maxLevel = 60, multiplier = 6},
+	{minLevel = 61, maxLevel = 80, multiplier = 4},
+	{minLevel = 81, maxLevel = 1000, multiplier = 2}
 }
