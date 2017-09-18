85bfbadf47cf2bb9f9934c2d117ab85d56425a81
Jesús Rafael Zúñiga Castro <Tatuy@users.noreply.github.com>
2017-09-17 23:18:26 -0700

    Remove unused files

--- data/spells/scripts mm/manarune.lua
+++
@@ -1,9 +0,0 @@
-
-function onCastSpell(cid, var)
-
-            doPlayerAddMana(cid, math.random(70, 110))
-            doSendAnimatedText(getPlayerPosition(cid), "Aaaah...", 198)
-            doSendMagicEffect(getPlayerPosition(cid), 12)
-return false
-	
-end
