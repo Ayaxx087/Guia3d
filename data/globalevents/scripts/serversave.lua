1d54c03edce307128b863c8d7b63187e01cad357
Jesús Rafael Zúñiga Castro <Tatuy@users.noreply.github.com>
2017-09-20 03:15:41 -0700

    Update serversave.lua
    Function optimization.

--- data/globalevents/scripts/serversave.lua
+++ data/globalevents/scripts/serversave.lua
@@ -1,41 +1,24 @@
-local shutdownAtServerSave = true
-local cleanMapAtServerSave = false
-
 local function serverSave()
-        if shutdownAtServerSave then
-                doSetGameState(GAME_STATE_SHUTDOWN)
-    else
-        doSetGameState(GAME_STATE_NORMAL)
-        end
-        if cleanMapAtServerSave then
-                cleanMap()
-        end
-        if doSaveServer(true) then
-		doTatuyBroadcastMessage("Server has been saved.")
-		else
-		doTatuyBroadcastMessage("Server could not be saved.")
-		end
+    doSaveServer(true)
+	doSetGameState(GAME_STATE_SHUTDOWN)
 end
 
-local function secondServerSaveWarning()
-        doTatuyBroadcastMessage("Server is saving game. The server will be open again in a moment.")
-		doSetGameState(GAME_STATE_CLOSED)
-		if doSaveServer(true) then
-			doTatuyBroadcastMessage("SAVED! the server will close in 10 seconds...")
-		else
-			doTatuyBroadcastMessage("Server could not be saved. Please report this to an GM.")
-		end
-        addEvent(serverSave, 10000)
+local function secondSaveWarning()
+    --broadcastMessage("Server is saving game in one minute. Please go to a safe place!", MESSAGE_STATUS_WARNING)
+	doTatuyBroadcastMessage("Server is saving game in one minute. Please go to a safe place!")
+    addEvent(serverSave, 60000)
 end
 
-local function firstServerSaveWarning()
-        doTatuyBroadcastMessage("Server is saving game in 2 minutes. Please go to a safe place.")
-        addEvent(secondServerSaveWarning, 120000)
+local function firstSaveWarning()
+    --broadcastMessage("Server is saving game in 3 minutes. Please go to a safe place!", MESSAGE_STATUS_WARNING)
+	doTatuyBroadcastMessage("Server is saving game in 3 minutes. Please go to a safe place!")
+    addEvent(secondSaveWarning, 120000)
 end
 
-function onTime(interval)
-        doTatuyBroadcastMessage("Server is saving game in 4 minutes. Please go to a safe place.")
-        doSetGameState(GAME_STATE_STARTUP)
-        addEvent(firstServerSaveWarning, 120000)
-        return not shutdownAtServerSave
+function onTime()
+	doSetGameState(GAME_STATE_CLOSED)
+    --broadcastMessage("Server is saving game in 5 minutes. Please go to a safe place!", MESSAGE_STATUS_WARNING)
+	doTatuyBroadcastMessage("Server is saving game in 5 minutes. Please go to a safe place!")
+    addEvent(firstSaveWarning, 120000)
+    return true
 end
