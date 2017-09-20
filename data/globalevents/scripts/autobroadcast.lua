4ffd2a913932f4de45440c960871dd2779de0dcc
Jesús Rafael Zúñiga Castro <Tatuy@users.noreply.github.com>
2017-09-20 06:35:38 -0700

    Update autobroadcast.lua
    Add !online command.

--- data/globalevents/scripts/autobroadcast.lua
+++ data/globalevents/scripts/autobroadcast.lua
@@ -1,6 +1,6 @@
 function onThink(interval, lastExecution)
     local MESSAGE = {
-		"[COMMANDS] !sellhouse - !buyhouse - !leavehouse - !houseinfo - !frags - !war - !bless.",
+		"[COMMANDS] !online - !frags - !bless - !war - !sellhouse - !buyhouse - !leavehouse - !houseinfo.",
 		"[TIBIA CLIENT UPDATED] http://uatibia.com/guia3d/download.php Use our Own Client to avoid debugs and can use all the functions of the server.",
 		"[SECURITY] Do not use the same passwords of others otservers, they can hack your account (use diferent accounts).",
 		"[RULES] http://uatibia.com/guia3d Read the rules to avoid punishments.",
